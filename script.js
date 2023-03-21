9d5ac702d43d252b6b8290a601e2b560f4554294
Adriel Melendez <adrielmelendez833@gmail.com>
2023-03-21 00:03:58 -0700

    Create working playRound function

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function getComputerChoice() {
         return Math.floor(Math.random() * max);
     }
 
-    const randomNumber = getRandomNumber(3);
+    //Store generated number in randomNumber
+    const randomNumber = getRandomNumber(3) + 1;
 
-    //Use random number to print an option
+    //Use random number to return an option
     if (randomNumber === 3) {
         return ("Scissors");
     } else if (randomNumber === 2) {
@@ -16,4 +17,42 @@ function getComputerChoice() {
         return ("Rock");
     }
 }
-const computerChoice = getComputerChoice();
+
+//Store computer's choice in computerSelection and player's choice in playerSelection
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
+//Play single round of Rock, Paper, Scissors
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return ("Game is a tie!");
+    } else {
+        if (playerSelection === "rock") {
+            if (computerSelection === "paper"){
+                console.log(computerSelection);
+                console.log(playerSelection);
+                return ("Computer wins!");
+            } else {
+                return ("Player wins!");
+            }
+        } else if (playerSelection === "paper") {
+            if (computerSelection === "scissors") {
+                return ("Computer wins!");
+            } else {
+                return ("Player wins!");
+            }
+        } else if (playerSelection === "scissors") {
+            if (computerSelection === "rock") {
+                return ("Computer wins!");
+            } else {
+                return ("Player wins!");
+            }
+        } else {
+            return ("Please enter a valid response: Rock, Paper, or Scissors")
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
